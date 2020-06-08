a4a1cfb4d37a186fd08f5eb93dc44d6e370c5bc7
pinheirochagas <ppinheirochagas@gmail.com>
2020-06-07 18:10:59 -0700

    final

--- play_sound_trials.m
+++ play_sound_trials.m
@@ -2,9 +2,10 @@ dir_code = '/Users/pinheirochagas/Pedro/Stanford/code/brain_music';
 data_fn = [dir_code filesep 'data.mat'];
 load(data_fn)
 
-% Select 
+% Select
 electrodes = [105, 15, 61, 81, 70];
-trials = data.trialinfo_all{1};     
+elect_regions = {'lateral occipital gyrus (LOG)', 'posterior inferior temporal gyrus (pITG)','anterior intraparietal sulcus (aIPS)','superior parietal lobe (SPL)','premotor cortex (PM)'};
+trials = data.trialinfo_all{1};
 trials_no = find(strcmp(trials.condNames, 'math') & trials.spike_hfb==false & trials.RT < 5 & trials.RT > 1);
 trials_final = trials(trials_no,:);
 [~,idx] = sort(trials_final.RT);
@@ -14,34 +15,68 @@ data_plot = squeeze(data.wave(trials_no,3,:));
 data_plot = data_plot(idx,:);
 
 % Preapare sound
-amp = 10; 
+amp = 10;
 fs = 20500;  % sampling frequency
 duration = 0.05;
 values = 0:1/fs:duration;
 
 % sound_all = [];
 % Transform data to sound
-for i = 1:12%size(data_plot,1) % take first 10 trials
-    RT_idx = (round(trials_final.RT(i)*data.fsample) + abs(min(data.time)*data.fsample));
-    times = 1:RT_idx;
-    data_plot_tmp = data_plot(i,:);
-    data_plot_tmp(times(end):(abs(data.time(1))+data.time(end))*data.fsample+1) = 0;
-    
-    data_plot_tmp(data_plot_tmp<=0) = 0;
-    peak_vals_trans = round(data_plot_tmp*100);
-    peak_vals_trans(peak_vals_trans==0) = 0;
-    peak_vals_trans = downsample(peak_vals_trans,10);
-    
-    b = [];
-    for iss = 1:length(peak_vals_trans)
-        peak_vals_trans(iss);
-        freq = peak_vals_trans(iss);
-        a = amp*sin(2*pi* freq*values);
-        a_all(iss,:) = a;
-        b = [b a]; %zeros(100,1)'
+
+for ielec = 1:length(electrodes)
+    data_plot = squeeze(data.wave(trials_no,ielec,:));
+    data_plot = data_plot(idx,:);
+
+    b_all = [];
+    for i = 1:10%:size(data_plot,1) % take first 10 trials
+        RT_idx = (round(trials_final.RT(i)*data.fsample) + abs(min(data.time)*data.fsample));
+        times = 1:RT_idx;
+        data_plot_tmp = data_plot(i,:);
+        data_plot_tmp(times(end):(abs(data.time(1))+data.time(end))*data.fsample+1) = 0;
+        
+        data_plot_tmp(data_plot_tmp<=0) = 0;
+        peak_vals_trans = round(data_plot_tmp*100);
+        peak_vals_trans(peak_vals_trans==0) = 1;
+        peak_vals_trans = downsample(peak_vals_trans,10);
+        
+        peak_vals_trans(peak_vals_trans<100) = 99999;  
+        
+        b = [];
+        for iss = 1:length(peak_vals_trans)
+            peak_vals_trans(iss);
+            freq = peak_vals_trans(iss);
+            a = amp*sin(2*pi* freq*values);
+            if freq == 99999
+                a(:) = 0;
+            else
+            end
+            a_all(iss,:) = a;
+            b = [b a]; %zeros(100,1)'
+        end
+        b_all = [b_all;b];
+        
+%         audiowrite(sprintf('%s/mmr_audio/mmr_audio_%s.wav',dir_code, num2str(i)),b,10000);
     end
-%     audiowrite(sprintf('%s/mmr_audio/mmr_audio_%s.wav',dir_code, num2str(i)),b,10000);
+    b_mean = mean(b_all);  
+    b_sum = sum(b_all);  
+    
+    %% Plot spectrogram of all trials
+%     subplot(length(electrodes),1,ielec)
+%     pspectrum(b_sum,10000,'spectrogram','TimeResolution',0.1,'OverlapPercent',0,'MinThreshold',-20);
+%     ylim([0 2])
+%     xlim([0 11])
+%     xlabel('Time')
+%     set(gca,'xtick',[])
+%     set(gca,'fontsize',16)
+%     title(elect_regions{ielec}) 
+%     
+%     colormap(viridis)
+    b_sum = bandpass(b_sum,[300 2000], 10000);
+    audiowrite(sprintf('%s/mmr_audio/mmr_audio_elect_%s.wav', dir_code, num2str(electrodes(ielec))),b_sum,10000);
 end
+fname = [dir_code filesep 'spectrogram_elecs.png'];
+savePNG(gcf,600,fname)
+
 
 figure('units', 'normalized', 'outerposition', [0 0 0.4 0.5])
 subplot(3,1,1)
@@ -80,10 +115,10 @@ for ie = 1:length(electrodes)
         [~,peaks] = findpeaks(data_plot_tmp+10*i);
         [peak_vals,~] = findpeaks(data_plot_tmp);
         peak_vals_avg(i,ie) = mean(peak_vals(peak_vals>0));
-%         plot(data.time(peaks), data_plot_tmp(peaks)+10*i, '.', 'Color', cols(i,:), 'MarkerSize', 6)
+        %         plot(data.time(peaks), data_plot_tmp(peaks)+10*i, '.', 'Color', cols(i,:), 'MarkerSize', 6)
         plot(data.time, data_plot_tmp+10*i, 'k', 'LineWidth', 1.5, 'Color', cols(i,:))
         plot(data.time(RT_idx), data_plot_tmp(RT_idx)+10*i, '.', 'MarkerSize', 5, 'Color', 'k')
-
+        
     end
     xlim([-0.250 5])
     xline(0, 'LineWidth', 2, 'Color', 'k')
